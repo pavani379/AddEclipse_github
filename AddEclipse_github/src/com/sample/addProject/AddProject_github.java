a8d4d45f3e990323881294cce4943893529f5ee5
=chinni <=pavanivatrapu379@gmail.com>
2020-03-24 08:49:23 +0530

    Second commit and simple file in the repo

---
+++ AddEclipse_github/src/com/sample/addProject/AddProject_github.java
@@ -0,0 +1,10 @@
+package com.sample.addProject;
+
+public class AddProject_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first sample example");
+	}
+
+}
